059dcc41f2b9d6f96a5d55f005c1f1dde4b09694
Gabriella Correia <gabriellacorreia@gec.inatel.br>
2020-04-14 17:45:09 -0300

    Aula 2: backend com express, nodemon, sqlite, rotas, cors, controllers, knex, migrations

---
+++ Server/src/routes.js
@@ -0,0 +1,38 @@
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request body: Corpo da requisição utilizado para alterar recursos
+ */
+
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
