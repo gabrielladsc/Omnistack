41ce5759fc5e100000cfb9cf3b5194e23ea9ff20
Gabriella Correia <gabriellacorreia@gec.inatel.br>
2020-04-15 11:31:33 -0300

    Aula 3: Tela de Login

--- Web/frontend/src/App.js
+++ Web/frontend/src/App.js
@@ -1,20 +1,12 @@
 import React, {useState} from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Logon from './pages/Logon';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  //Array [valor, funcao de atualizacao]
-
-  function increment(){
-    setCounter(counter + 1);
-  }
-  return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+  return(
+    <Logon />
   );
 }
 
